411ee4b8340c962642900d335b1a3267edfa77bd
kilax9276 <kilax123@gmail.com>
2019-10-10 17:49:24 +0300

    tiny fix

--- bornyear.py
+++ bornyear.py
@@ -1,15 +1,15 @@
 
 print('Скажите а вы знаете когда родился А.С. Пушкин?')
-answer_year = input('Сначала введите год')
+answer_year = input('Сначала введите год: ')
 
 if int(answer_year) != 1799:
     print('Неверный год')
     exit(0)
 
-answer_month = input('Теперь введите месяц')
-answer_day = input('А теперь введите день')
+answer_month = input('Теперь введите месяц: ')
+answer_day = input('А теперь введите день: ')
 
-if int(answer_month) != 7 and int(answer_day) != 6
+if int(answer_month) != 7 and int(answer_day) != 6:
     print('Неверный день рождения')
 else:
     print('Верно')
